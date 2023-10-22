7784ce95c3840971f8dd1a2cb4eca0f437107c19
Shahan malik <shahanmalikkhan@gmail.com>
2023-10-23 00:04:10 +0500

    assgnment 2

---
+++ Project1/src/LabAssignment2/Main.java
@@ -0,0 +1,13 @@
+package LabAssignment2;
+
+public class Main {
+    public static void main(String[] args) {
+        SInglyLinkedList singly = new SInglyLinkedList();
+        singly.insert(0);
+        singly.insert(1);
+        singly.insert(2);
+        singly.insert(3);
+        singly.insertIndoubleLinkedList();
+        singly.show();
+    }
+}
